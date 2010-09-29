8560b7a99528cce47690632f4f9c860c0091fade
Pablo Ruiz Garcia <pablo.ruiz@gmail.com>
2010-09-29 15:22:28 +0200

    Avoided ServiceController.GetServices() call on mono/unix

---
+++ src/Topshelf/EnviromentHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Topshelf
+{
+	public static class EnviromentHelper
+	{
+		public static bool RunningOnMono
+		{
+			get
+			{
+				Type t = Type.GetType("Mono.Runtime");
+				if (t != null)
+					return true;
+
+				return false;
+			}
+		}
+
+		public static bool RunninOnUnix
+		{
+			get
+			{
+				int p = (int)Environment.OSVersion.Platform;
+				return ((p == 4) || (p == 6) || (p == 128));
+			}
+		}
+	}
+}
